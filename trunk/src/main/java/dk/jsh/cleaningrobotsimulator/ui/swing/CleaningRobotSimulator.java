9a7fb0f8b5fe0d3923009524fc93aeabd3dc7df6
jan.sch.hansen <jan.sch.hansen@e1ec02f1-7254-4684-b7ac-b6e77e6ef5d3>
2010-04-14 18:20:11 +0000

    Rename from Desktopapplication1 to cleaningrobotsimulator.

---
+++ trunk/src/main/java/dk/jsh/cleaningrobotsimulator/ui/swing/CleaningRobotSimulator.java
@@ -0,0 +1,44 @@
+/*
+ * CleaningRobotSimulator.java
+ */
+
+package dk.jsh.cleaningrobotsimulator.ui.swing;
+
+import org.jdesktop.application.Application;
+import org.jdesktop.application.SingleFrameApplication;
+
+/**
+ * The main class of the application.
+ */
+public class CleaningRobotSimulator extends SingleFrameApplication {
+
+    /**
+     * At startup create and show the main frame of the application.
+     */
+    @Override protected void startup() {
+        show(new View(this));
+    }
+
+    /**
+     * This method is to initialize the specified window by injecting resources.
+     * Windows shown in our application come fully initialized from the GUI
+     * builder, so this additional configuration is not needed.
+     */
+    @Override protected void configureWindow(java.awt.Window root) {
+    }
+
+    /**
+     * A convenient static getter for the application instance.
+     * @return the instance of CleaningRobotSimulator
+     */
+    public static CleaningRobotSimulator getApplication() {
+        return Application.getInstance(CleaningRobotSimulator.class);
+    }
+
+    /**
+     * Main method launching the application.
+     */
+    public static void main(String[] args) {
+        launch(CleaningRobotSimulator.class, args);
+    }
+}
