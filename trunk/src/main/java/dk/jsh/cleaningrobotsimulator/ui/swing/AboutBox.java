ef2331b543f9f038ffb8034a53a4984b4f2a54a9
jan.sch.hansen <jan.sch.hansen@e1ec02f1-7254-4684-b7ac-b6e77e6ef5d3>
2010-04-14 18:39:43 +0000

    Minor change in about box.

--- trunk/src/main/java/dk/jsh/cleaningrobotsimulator/ui/swing/AboutBox.java
+++ trunk/src/main/java/dk/jsh/cleaningrobotsimulator/ui/swing/AboutBox.java
@@ -98,7 +98,7 @@ public class AboutBox extends javax.swing.JDialog {
                             .add(appVendorLabel)
                             .add(appHomepageLabel)))
                     .add(org.jdesktop.layout.GroupLayout.LEADING, appTitleLabel)
-                    .add(org.jdesktop.layout.GroupLayout.LEADING, appDescLabel, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 266, Short.MAX_VALUE)
+                    .add(org.jdesktop.layout.GroupLayout.LEADING, appDescLabel, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 346, Short.MAX_VALUE)
                     .add(closeButton))
                 .addContainerGap())
         );
@@ -122,7 +122,7 @@ public class AboutBox extends javax.swing.JDialog {
                 .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.BASELINE)
                     .add(homepageLabel)
                     .add(appHomepageLabel))
-                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED, 19, Short.MAX_VALUE)
+                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED, 33, Short.MAX_VALUE)
                 .add(closeButton)
                 .addContainerGap())
         );
