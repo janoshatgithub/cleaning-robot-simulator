33d572e75550045600794894ca2e349a50e3de3c
jan.sch.hansen@gmail.com <jan.sch.hansen@gmail.com@e1ec02f1-7254-4684-b7ac-b6e77e6ef5d3>
2010-05-05 11:46:18 +0000

    Format code.

--- trunk/src/main/java/dk/jsh/cleaningrobotsimulator/concurrent/Constants.java
+++ trunk/src/main/java/dk/jsh/cleaningrobotsimulator/concurrent/Constants.java
@@ -7,12 +7,15 @@ import java.text.SimpleDateFormat;
  * @author Jan S. Hansen
  */
 public class Constants {
+
     public final static int MAX_ROWS = 10;
     public final static int MAX_COLUMNS = 10;
     public final static int MAX_DIRTY_FIELDS = 10;
     public final static int MAX_CLEANED_FIELDS = 5;
-    public final static SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm:ss");
+    public final static SimpleDateFormat timeFormat =
+            new SimpleDateFormat("HH:mm:ss");
 
     //Empty private constructor to prevent that this class can be instantiated.
-    private Constants() {}
+    private Constants() {
+    }
 }
